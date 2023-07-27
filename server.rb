5411e0635bf4004fc6cb49bd5fc748b030b3cff6
binarygit <inscription4@outlook.com>
2023-07-27 16:01:49 +0545

    Make new page the root page

--- server.rb
+++ server.rb
@@ -4,6 +4,9 @@ require 'webrick'
 
 root = File.expand_path Dir.pwd
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+# Make new.html the root page
+server.config[:DirectoryIndex] << "new.html"
+
 trap 'INT' do server.shutdown end
 
 server.start

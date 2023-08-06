654a3329ffc0d7e60d2446a6f773416331aef8d1
binarygit <inscription4@outlook.com>
2023-08-06 11:45:21 +0545

    Create continue action for GameServer

--- game_server.rb
+++ game_server.rb
@@ -8,14 +8,19 @@ class GameServer
     @form = document.getElementById "main-form"
     current_url = JS.global[:window][:location][:href]
     if current_url.to_s.match?(/breaker=true/)
-      @code_maker = ComputerClient.new
-      @code_maker.code_breaker = false
-      #@code_breaker = HumanClient.new(@code_breaker = true)
+      @code_maker   = ComputerClient.new
+      @code_breaker = HumanClient.new(code_breaker: true)
+      @code_breaker.game_server = self
     end
   end
 
   def start
     @code_maker.make_move
+    @code_breaker.make_move
+  end
+
+  def continue(move)
+    puts move.value
   end
 end
 
